bfb5039ddd5badaeba056ef0b44b678ad2273e3a
Kyle Bruns <Kylecbruns@live.com>
2022-03-04 19:35:13 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      serial: '',
+      species: '',
+      group: '',
+      type: '',
+      moisture: '',
+      PH: '',
+      temperature: '',
+    }
+  });
